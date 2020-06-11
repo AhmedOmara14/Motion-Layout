8e08810d823a3cc4061809f4ad7ab5a40d4d30c6
AhmedOmara14 <ahmedomaea53@gmail.com>
2020-06-11 07:57:09 -0700

    intial project

---
+++ app/src/main/java/com/example/motionlayout/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.motionlayout.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
